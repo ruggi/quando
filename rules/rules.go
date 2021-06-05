b2a5fff2db04bd4ac31e1df9d89c16ca6a961ef0
Federico Ruggi <federico.ruggi@hey.com>
2021-06-05 11:41:43 +0200

    use named groups

--- rules/rules.go
+++ rules/rules.go
@@ -9,8 +9,8 @@ type Rule struct {
 	Name string
 	Re   *regexp.Regexp
 
-	TimeFn     func(t time.Time, s []string) (time.Time, error)
-	DurationFn func(t time.Duration, s []string) (time.Duration, error)
+	TimeFn     func(t time.Time, matches map[string]string) (time.Time, error)
+	DurationFn func(t time.Duration, matches map[string]string) (time.Duration, error)
 
 	Disabled bool
 }
